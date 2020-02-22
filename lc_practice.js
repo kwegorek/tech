00f1b7482834e9249524893001a975b197f6054e
kwegorek <50807299+kwegorek@users.noreply.github.com>
2020-02-22 17:39:36 -0500

    mergeTwoLists_lc

--- lc_practice.js
+++ lc_practice.js
@@ -338,3 +338,30 @@ var isPowerOfThree = function(n) {
 
    
 };
+
+var mergeTwoLists = function(l1, l2) {
+  let l3 = new ListNode(1)
+  let l3head = l3
+
+  while (l1 !== null && l2 !== null) {
+    if (l1.val < l2.val) {
+      l3.next = new ListNode(l1.val)
+      l1 = l1.next
+      l3 = l3.next
+    } else {
+      l3.next = new ListNode(l2.val)
+      l2 = l2.next
+      l3 = l3.next
+    }
+  }
+
+  if (l1 === null && l2 !== null) {
+    l3.next = l2
+  }
+
+  if (l1 !== null && l2 === null) {
+    l3.next = l1
+  }
+
+  return l3head.next
+}
