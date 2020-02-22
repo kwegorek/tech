2f9d81ad65077a7f48f6ede10045606f1e86b7da
kwegorek <50807299+kwegorek@users.noreply.github.com>
2020-02-21 19:24:56 -0500

    anagram_lc

--- lc_practice.js
+++ lc_practice.js
@@ -239,3 +239,26 @@ var majorityElement = function(nums) {
     return Object.keys(countElements).find(key => countElements[key] > condition)
     
 };
+
+var isAnagram = function(s, t) {
+    
+    let dic =  {}
+    
+    if(s.length === t.length){
+    
+    for(let i =0; i < s.length; i++){
+        
+        if(!dic[s[i]]){
+            dic[s[i]] = 1
+        }else {
+            
+            dic[s[i]] = +1
+        }
+        
+        
+    }
+
+    return dic
+
+    
+    }}
