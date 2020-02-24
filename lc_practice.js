8ce49a91d9c29248a88d47700d116e05e9f7d9ad
kwegorek <50807299+kwegorek@users.noreply.github.com>
2020-02-24 11:22:53 -0500

    revetse_ll

--- lc_practice.js
+++ lc_practice.js
@@ -391,3 +391,20 @@ i))
     
     
 };
+
+var reverseList = function(head) {
+  var node = head
+  var prev = null
+
+  while (node) {
+    var temp = node.next
+    node.next = prev
+    prev = node
+
+    if (!temp) {
+      break
+    }
+    node = temp
+  }
+  return node
+}
